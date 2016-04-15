be480f209b4783eaa1583fc5e3ba68455f51a55f
khill <katherine.hill.c9@gmail.com>
2016-04-15 14:52:34 -0500

    Added basic ability to get all groups from REST interface

---
+++ src/YouTrackSharp/Admin/Group.cs
@@ -0,0 +1,24 @@
+﻿using JsonFx.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace YouTrackSharp.Admin
+{
+	class Group
+	{
+		public string Name { get; set; }
+		public string Url { get; set; }
+
+		[JsonIgnore]
+		public string Id
+		{
+			get
+			{
+				return HttpUtility.UrlDecode(Url.Substring(Url.LastIndexOf('/') + 1));
+			}
+		}
+	}
+}
---
+++ src/YouTrackSharp/Admin/GroupManagement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YouTrackSharp.Infrastructure;
+
+namespace YouTrackSharp.Admin
+{
+	class GroupManagement
+	{
+		readonly IConnection _connection;
+		public GroupManagement(IConnection connection)
+		{
+			_connection = connection;
+		}
+
+		public IEnumerable<Group> GetAllGroups()
+		{
+			IEnumerable<Group> groups = _connection.Get<IEnumerable<Group>>("admin/group");
+
+			return groups;
+		}
+	}
+}
