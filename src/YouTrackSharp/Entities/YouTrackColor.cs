107803a152b8bf9b3abd9aecfa2b8591839dc3e5
Maarten Balliauw <maarten@maartenballiauw.be>
2017-07-20 09:45:03 +0200

    Add color indices utility

---
+++ src/YouTrackSharp/Entities/YouTrackColor.cs
@@ -0,0 +1,19 @@
+﻿// ReSharper disable once CheckNamespace
+namespace YouTrackSharp
+{
+    /// <summary>
+    /// YouTrack color representation that has a foreground and background color in HEX format (e.g. #112233).
+    /// </summary>
+    public sealed class YouTrackColor
+    {
+        /// <summary>
+        /// Foreground color in HEX format (e.g. #112233).
+        /// </summary>
+        public string Foreground { get; internal set; }
+
+        /// <summary>
+        /// Background color in HEX format (e.g. #112233).
+        /// </summary>
+        public string Background { get; internal set; }
+    }
+}
