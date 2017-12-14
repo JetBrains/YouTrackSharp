97d6e2bb678180971449d9d5183ae99c0dcc6048
Christian Nylund <chny@trustit.se>
2017-12-14 08:06:25 +0100

    XML docs

--- src/YouTrackSharp/AgileBoards/Field.cs
+++ src/YouTrackSharp/AgileBoards/Field.cs
@@ -2,11 +2,20 @@
 
 namespace YouTrackSharp.AgileBoards
 {
+    /// <summary>
+    /// A class that represents a YouTrack field used in the context of an agile board
+    /// </summary>
     public class Field
     {
+        /// <summary>
+        /// Gets or sets the name of the field
+        /// </summary>
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets the localized name of the field
+        /// </summary>
         [JsonProperty("localizedName")]
         public string LocalizedName { get; set; }
     }
