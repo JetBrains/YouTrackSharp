c0828910717100207490b6cfe8716d03d133fc21
Christian Nylund <chny@trustit.se>
2017-12-14 08:04:10 +0100

    XML docs and constructor that initializes the VisibleValues collection

--- src/YouTrackSharp/AgileBoards/ColumnSettings.cs
+++ src/YouTrackSharp/AgileBoards/ColumnSettings.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 
 namespace YouTrackSharp.AgileBoards
 {
+    /// <summary>
+    /// A class that represents the column settings of a board
+    /// </summary>
     public class ColumnSettings
     {
+        /// <summary>
+        /// Creates an instance of the <see cref="ColumnSettings"/> class
+        /// </summary>
+        public ColumnSettings()
+        {
+            VisibleValues = new List<VisibleValue>();
+        }
+
+        /// <summary>
+        /// Gets or sets the field used to determine the columns of a board
+        /// </summary>
         [JsonProperty("field")]
         public Field Field { get; set; }
 
+        /// <summary>
+        /// A list of the <see cref="VisibleValues"/> comprising the columns of a board
+        /// </summary>
         [JsonProperty("visibleValues")]
         public ICollection<VisibleValue> VisibleValues { get; set; }
     }
