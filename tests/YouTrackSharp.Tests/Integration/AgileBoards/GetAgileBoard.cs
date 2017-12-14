3b4aad5b6de58e55f14a4db380b6c68765618ecb
Christian Nylund <chny@trustit.se>
2017-12-14 10:49:19 +0100

    Tests for GetAgileBOards, GetAgileBoard and GetSprint

---
+++ tests/YouTrackSharp.Tests/Integration/AgileBoards/GetAgileBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using YouTrackSharp.Tests.Infrastructure;
+
+namespace YouTrackSharp.Tests.Integration.AgileBoards
+{
+    public partial class AgileBoardServiceTests
+    {
+        public class GetAgileBoard
+        {
+            [Fact]
+            public async Task Valid_Connection_Returns_Agile_Board()
+            {
+                // Arrange
+                var connection = Connections.Demo1Token;
+                var service = connection.CreateAgileBoardService();
+
+                // Act
+                var demoBoard = await service.GetAgileBoard(DemoBoardId);
+
+                // Assert
+                Assert.NotNull(demoBoard);
+
+                Assert.Equal(demoBoard.Id, DemoBoardId);
+                Assert.Equal(demoBoard.Name, DemoBoardName);
+                Assert.NotNull(demoBoard.Id);
+                Assert.NotNull(demoBoard.Name);
+                Assert.NotNull(demoBoard.ColumnSettings);
+                Assert.NotNull(demoBoard.Backlog);
+                Assert.NotNull(demoBoard.Projects);
+                Assert.NotNull(demoBoard.Sprints);
+            }
+
+            [Fact]
+            public async Task Invalid_Connection_Throws_UnauthorizedConnectionException()
+            {
+                // Arrange
+                var service = Connections.UnauthorizedConnection.CreateAgileBoardService();
+
+                // Act & Assert
+                await Assert.ThrowsAsync<UnauthorizedConnectionException>(
+                    async () => await service.GetAgileBoard(DemoBoardId));
+            }
+        }
+    }
+}
---
+++ tests/YouTrackSharp.Tests/Integration/AgileBoards/GetAgileBoards.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using YouTrackSharp.Tests.Infrastructure;
+
+namespace YouTrackSharp.Tests.Integration.AgileBoards
+{
+    public partial class AgileBoardServiceTests
+    {
+        public class GetAgileBoards
+        {
+            [Fact]
+            public async Task Valid_Connection_Returns_Agile_Boards()
+            {
+                // Arrange
+                var connection = Connections.Demo1Token;
+                var service = connection.CreateAgileBoardService();
+
+                // Act
+                var result = await service.GetAgileBoards();
+
+                // Assert
+                Assert.NotNull(result);
+                Assert.NotEmpty(result);
+
+                var demoBoard = result.FirstOrDefault();
+                Assert.NotNull(demoBoard);
+                Assert.NotNull(demoBoard.Id);
+                Assert.NotNull(demoBoard.Name);
+                Assert.NotNull(demoBoard.ColumnSettings);
+                Assert.NotNull(demoBoard.Backlog);
+                Assert.NotNull(demoBoard.Projects);
+                Assert.NotNull(demoBoard.Sprints);
+            }
+
+            [Fact]
+            public async Task Invalid_Connection_Throws_UnauthorizedConnectionException()
+            {
+                // Arrange
+                var service = Connections.UnauthorizedConnection.CreateAgileBoardService();
+
+                // Act & Assert
+                await Assert.ThrowsAsync<UnauthorizedConnectionException>(
+                    async () => await service.GetAgileBoards());
+            }
+        }
+    }
+}
