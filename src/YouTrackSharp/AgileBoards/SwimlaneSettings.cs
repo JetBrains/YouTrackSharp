925779756d4d82f3e526e14552bd3d85b3978821
Christian Nylund <chny@trustit.se>
2017-12-14 08:21:23 +0100

    XML docs and a constructor that initializes tha Values collection

--- src/YouTrackSharp/AgileBoards/SwimlaneSettings.cs
+++ src/YouTrackSharp/AgileBoards/SwimlaneSettings.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 
 namespace YouTrackSharp.AgileBoards
 {
+    /// <summary>
+    /// A class that represents the swimlane settings of a board
+    /// </summary>
     public class SwimlaneSettings
     {
+        /// <summary>
+        /// Creates a new <see cref="SwimlaneSettings"/> object
+        /// </summary>
+        public SwimlaneSettings()
+        {
+            Values = new List<string>();
+        }
+
+        /// <summary>
+        /// Gets or sets the type used to determine how to represent swimlanes on a board
+        /// </summary>
         [JsonProperty("field")]
         public Field Field { get; set; }
 
+        /// <summary>
+        /// Gets or sets the default card type of a board
+        /// </summary>
         [JsonProperty("defaultCardType")]
         public string DefaultCardType { get; set; }
 
+        /// <summary>
+        /// Gets an <see cref="ICollection{string}"/> of the valus use to represent swimlanes
+        /// </summary>
         [JsonProperty("values")]
         public ICollection<string> Values { get; set; }
     }
