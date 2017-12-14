6daa2a8bcb8d49c4c3f807ab5db841d2663722d2
Christian Nylund <chny@trustit.se>
2017-12-14 08:15:41 +0100

    XML docs and changed the date types to DateTimeOffset

--- src/YouTrackSharp/AgileBoards/Sprint.cs
+++ src/YouTrackSharp/AgileBoards/Sprint.cs
@@ -4,20 +4,35 @@ using YouTrackSharp.Json;
 
 namespace YouTrackSharp.AgileBoards
 {
+    /// <summary>
+    /// A class that represents a sprint
+    /// </summary>
     public class Sprint
     {
+        /// <summary>
+        /// Gets or sets the id used by YouTrack to identify a sprint
+        /// </summary>
         [JsonProperty("id")]
         public string Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets the name of the sprint.
+        /// </summary>
         [JsonProperty("version")]
         public string Version { get; set; }
 
+        /// <summary>
+        /// Gets or sets the start date of the sprint
+        /// </summary>
         [JsonConverter(typeof(UnixDateTimeOffsetConverter))]
         [JsonProperty("start")]
-        public DateTime? Start { get; set; }
+        public DateTimeOffset? Start { get; set; }
 
+        /// <summary>
+        /// Gets or sets the last day of the sprint
+        /// </summary>
         [JsonConverter(typeof(UnixDateTimeOffsetConverter))]
         [JsonProperty("finish")]
-        public DateTime? Finish { get; set; }
+        public DateTimeOffset? Finish { get; set; }
     }
 }
