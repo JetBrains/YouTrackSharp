adeaa30428c8e8d4ccd1e5366229bc571fa96745
Christian Nylund <chny@trustit.se>
2017-12-19 08:05:03 +0100

    Unit test for CreateAgileBOard

---
+++ tests/YouTrackSharp.Tests/Integration/AgileBoards/CreateAgileBoard.cs
@@ -0,0 +1,41 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using YouTrackSharp.AgileBoards;
+using YouTrackSharp.Tests.Infrastructure;
+
+namespace YouTrackSharp.Tests.Integration.AgileBoards
+{
+    public partial class AgileBoardServiceTests
+    {
+        public class CreateAgileBoard
+        {
+            /*
+             * It is not feasible at this point to write a unit test for the creation of a board 
+             * since there is no way to remove it automatically after the test has run. 
+             */
+
+            [Fact]
+            public async Task Valid_Connection_Throws_HttpRequestException_On_Bad_Parameter()
+            {
+                // Arrange
+                var service = Connections.Demo1Token.CreateAgileBoardService();
+
+                // Act & Assert
+                await Assert.ThrowsAsync<HttpRequestException>(
+                    async () => await service.CreateAgileBoard(new AgileSettings()));
+            }
+
+            [Fact]
+            public async Task Invalid_Connection_Throws_UnauthorizedConnectionException()
+            {
+                // Arrange
+                var service = Connections.UnauthorizedConnection.CreateAgileBoardService();
+
+                // Act & Assert
+                await Assert.ThrowsAsync<UnauthorizedConnectionException>(
+                    async () => await service.CreateAgileBoard(new AgileSettings { Name = "Test Board" }));
+            }
+        }
+    }
+}
