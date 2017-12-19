c5fd678076472a8a128fbd82998872ce7cb3ee32
Christian Nylund <chny@trustit.se>
2017-12-19 10:19:34 +0100

    Unit Test Valid_Connection_Creates_Agile_Board added

--- tests/YouTrackSharp.Tests/Integration/AgileBoards/CreateAgileBoard.cs
+++ tests/YouTrackSharp.Tests/Integration/AgileBoards/CreateAgileBoard.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 using YouTrackSharp.AgileBoards;
@@ -10,10 +12,44 @@ namespace YouTrackSharp.Tests.Integration.AgileBoards
     {
         public class CreateAgileBoard
         {
-            /*
-             * It is not feasible at this point to write a unit test for the creation of a board 
-             * since there is no way to remove it automatically after the test has run. 
-             */
+            [Fact(Skip = "Skip test until the API supports removing the created board to avoid cluttering the test environment with hundreds of boards")]
+            public async Task Valid_Connection_Creates_Agile_Board()
+            {
+                // Arrange
+                var connection = Connections.Demo1Token;
+                var service = connection.CreateAgileBoardService();
+
+                // Act
+                var projects = new List<Project> { new Project { Id = "TP" } };
+                var columnSettings = new ColumnSettings
+                {
+                    Field = new Field { Name = "State" }
+                };
+                var agileSettings = new AgileSettings
+                {
+                    Name = "Test Board",
+                    Projects = projects,
+                    ColumnSettings = columnSettings
+
+                };
+                var boardId = await service.CreateAgileBoard(agileSettings);
+                var newBoard = await service.GetAgileBoard(boardId);
+
+                // Assert
+                Assert.NotNull(boardId);
+                Assert.NotNull(newBoard);
+                Assert.NotNull(newBoard.Name);
+                Assert.NotNull(newBoard.Projects);
+                Assert.NotNull(newBoard.ColumnSettings);
+                Assert.NotNull(newBoard.ColumnSettings.Field);
+                Assert.Equal("Test Board", newBoard.Name);
+                Assert.Equal("State", newBoard.ColumnSettings.Field.Name);
+                Assert.Contains(newBoard.Projects, p=>p.Id=="TP");
+
+                //Clean up
+                // Remove the newly created board (currently not supported)
+
+            }
 
             [Fact]
             public async Task Valid_Connection_Throws_HttpRequestException_On_Bad_Parameter()
