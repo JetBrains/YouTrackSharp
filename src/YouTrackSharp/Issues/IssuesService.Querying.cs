e75d7594f29f73819a1c69cf6dd2e65abca69a4d
Maarten Balliauw <maarten@maartenballiauw.be>
2019-10-15 12:52:11 +1100

    UserManagementService .CreateUser replaces ' ' with '%20' #90

--- src/YouTrackSharp/Issues/IssuesService.Querying.cs
+++ src/YouTrackSharp/Issues/IssuesService.Querying.cs
@@ -85,8 +85,8 @@ namespace YouTrackSharp.Issues
 
             response.EnsureSuccessStatusCode();
 
-            var wrapper =
-                JsonConvert.DeserializeObject<IssueCollectionWrapper>(await response.Content.ReadAsStringAsync());
+            var wrapper = JsonConvert.DeserializeObject<IssueCollectionWrapper>(
+                await response.Content.ReadAsStringAsync());
             return wrapper.Issues;
         }
 
