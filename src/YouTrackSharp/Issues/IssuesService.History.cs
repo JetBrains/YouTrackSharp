4448bafe1776ab5ac04d29d0fe77898e3ad162e5
Roman Kolobov <Roman.Kolobov@jetbrains.com>
2023-03-18 01:58:13 +0200

    make retrieving vcs changes optional #125

--- src/YouTrackSharp/Issues/IssuesService.History.cs
+++ src/YouTrackSharp/Issues/IssuesService.History.cs
@@ -8,15 +8,18 @@ namespace YouTrackSharp.Issues
     public partial class IssuesService
     {
         private const string ACTIVITIES_CATEGORIES =
-            "AttachmentsCategory,CustomFieldCategory,DescriptionCategory,IssueResolvedCategory,LinksCategory,ProjectCategory,IssueVisibilityCategory,SprintCategory,SummaryCategory,TagsCategory,VcsChangeCategory";
+            "AttachmentsCategory,CustomFieldCategory,DescriptionCategory,IssueResolvedCategory" +
+            ",LinksCategory,ProjectCategory,IssueVisibilityCategory,SprintCategory,SummaryCategory,TagsCategory";
         
         /// <inheritdoc />
-        public async Task<IEnumerable<Change>> GetChangeHistoryForIssue(string issueId, string categories = ACTIVITIES_CATEGORIES)
+        public async Task<IEnumerable<Change>> GetChangeHistoryForIssue(string issueId, bool vcsHistory = false)
         {
             if (string.IsNullOrEmpty(issueId))
             {
                 throw new ArgumentNullException(nameof(issueId));
             }
+
+            var categories = ACTIVITIES_CATEGORIES + (vcsHistory ? ",VcsChangeCategory" : "");
 
             var client = await _connection.GetAuthenticatedApiClient();
             var response = await client.IssuesActivitiesGetAsync(issueId,
